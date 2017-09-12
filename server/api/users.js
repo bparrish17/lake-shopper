1b9153d49758e101919569d964d8668069690760
Alex Press <press.alex@gmail.com>
2017-09-12 19:08:50 -0400

    updated user routes

--- server/api/users.js
+++ server/api/users.js
@@ -17,8 +17,39 @@ router.get('/', (req, res, next) => {
 
 // create users
 
+router.get('/:id', (req, res, next) => {
+  var currentId = req.params.id;
+
+  User.findOne({ where: {id: currentId}})
+    .then(user => res.json(user))
+    .catch(next)
+})
+
 router.post('/', function(req, res, next){
   User.create(req.body)
     .then(result => res.send(result))
+    .catch(next);
+});
+
+router.put('/:id', function(req, res, next){
+  var currentId = req.params.id;
+
+  User.findById(currentId)
+    .then(user => {
+      if (user) {
+        user.update(req.body, { returning: true })
+      } else {
+        res.sendStatus(404)
+      }
+    })
+    .then(updatedUser => res.json(updatedUser[1]))
+    .catch(next);
+});
+
+router.delete('/:id', function(req, res, next){
+  var currentId = req.params.id;
+
+  User.destroy({ where: { id: currentId })
+    .then(result => res.sendStatus(204))
     .catch(next);
 });
