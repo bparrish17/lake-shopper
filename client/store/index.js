3e995e514b294829658836226fbc0df5a2e3f816
David Eiber <davideiber@gmail.com>
2017-09-13 16:23:07 -0400

    Don't merge

--- client/store/index.js
+++ client/store/index.js
@@ -2,6 +2,7 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import user from './user'
+import category from './category'
 
 const reducer = combineReducers({user})
 const middleware = applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
@@ -9,3 +10,4 @@ const store = createStore(reducer, middleware)
 
 export default store
 export * from './user'
+export * from './category'
