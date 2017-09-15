c8607ccc36faa12a8eb42d34f3a2ec8f95db4f76
Brian Parrish <brian.parrish3739@gmail.com>
2017-09-14 22:24:58 -0400

    front end view cart functaionality

--- client/components/ViewCart.js
+++ client/components/ViewCart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {NavLink} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import store from '../store';
 import {connect} from 'react-redux';
 import SingleProduct from './singleProduct'
@@ -11,16 +12,24 @@ const ViewCart = (props) => {
             <div id="temp">
             </div>
             <div className="row container-fluid">
-                <h1>Your Cart</h1>
+                <div className="col-xs-8">
+                <h1 id="your-cart-header">Your Cart</h1>
                 {
                     props.cart.map(product => {
                         return (
-                            <div className="col-xs-3" key={product.id}>
-                                <SingleProduct product={product}/>
-                            </div>
+                            <ul className="cart-item-list list-group" key={product.id}>
+                                <li className="cart-item-name list-group-item">{product.name}</li>
+                                <li className="cart-quantity-input input-group">
+                                    <span className="input-group-addon">#</span>
+                                        <input type="text" className="input-quantity form-control" aria-label="Change Quantity"></input>
+                                    <span className="input-group-addon">Edit Quantity</span>
+                                </li>
+                                <li className="cart-item-delete btn btn-danger remove btn-circle">Remove Item</li>
+                            </ul>
                         )
                     })
                 }
+                </div>
             </div>
         </div>
     );
