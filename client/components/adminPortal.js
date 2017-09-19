b3ecf06a5fafcf10b871431427749c1ad122cd27
Alex Press <press.alex@gmail.com>
2017-09-19 10:20:21 -0400

    fixed errors on admin page, temporarily commented out some of backend auth since im not a logged in user and need to see whats going on

--- client/components/adminPortal.js
+++ client/components/adminPortal.js
@@ -2,110 +2,120 @@ import React, { Component } from 'react'
 import {NavLink, withRouter} from 'react-router-dom'
 // import store from '../store'
 import {connect} from 'react-redux'
+import {getProductsThunk, fetchUsers, fetchOrders} from '../store'
 
-function AdminPortal (props) {
-  const {products, orders, users} = props
 
-  console.log('*** ADMIN PORTAL ***')
-  console.log(props)
-  console.log('git')
+class AdminPortal extends Component {
+  componentDidMount () {
+    this.props.loadInitialData()
+  }
 
-  return (
-    <div>
-      <font size='5'>Users</font>
-      <table id="userTable" className="table">
-        <thead>
-          <tr>
-            <th>ID #</th>
-            <th>Email</th>
-            <th>Admin Status</th>
-            <th>Update</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/*
-            users.map(user => {
-              return (
-                <tr key={user.id}>
-                  <th scope="row">{user.id}</th>
-                  <td>{user.email}</td>
-                  <td>{user.isAdmin}</td>
-                  <td>
-                    <NavLink to={`users/${user.id}`} >
-                      <button type="button" className="btn btn-outline-danger">Update</button>
-                    </NavLink>
-                  </td>
-                </tr>
-              )
-            })
-          */}
-        </tbody>
-      </table>
 
-      <font size='5'>Products</font>
-      <table id="productTable" className="table">
-        <thead>
-          <tr>
-            <th>ID #</th>
-            <th>Name</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Update</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            props.products.map(product => {
-              return (
-                <tr key={product.id}>
-                  <th scope="row">{product.id}</th>
-                  <td>{product.name}</td>
-                  <td>{product.price}</td>
-                  <td>{product.quantity}</td>
-                  <td>
-                    <NavLink to={`product/${product.id}/edit`} >
-                      <button type="button" className="btn btn-outline-danger">Update</button>
-                    </NavLink>
-                  </td>
-                </tr>
-              )
-            })
-          }
-        </tbody>
-      </table>
+  render () {
 
-      <font size='5'>Orders</font>
-      <table id="orderTable" className="table">
-        <thead>
-          <tr>
-            <th>ID #</th>
-            <th>Status</th>
-            <th>Checkout Date</th>
-            <th>Update</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            orders.map(order => {
-              return (
-                <tr key={order.id}>
-                  <th scope="row">{order.id}</th>
-                  <td>{order.status}</td>
-                  <td>{order.checkoutDate}</td>
-                  <td>
-                    <NavLink to={`orders/${order.id}/edit`} >
-                      <button type="button" className="btn btn-outline-danger">Update</button>
-                    </NavLink>
-                  </td>
-                </tr>
-              )
-            })
-          }
-        </tbody>
-      </table>
-    </div>
-  )
+    console.log('*** ADMIN PORTAL ***')
+    console.log(this.props)
+    console.log('git')
+
+    // const {products, orders, users} = props
+
+    return (
+      <div>
+        <font size='5'>Users</font>
+        <table id="userTable" className="table">
+          <thead>
+            <tr>
+              <th>ID #</th>
+              <th>Email</th>
+              <th>Admin Status</th>
+              <th>Update</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.props.users.map(user => {
+                return (
+                  <tr key={user.id}>
+                    <th scope="row">{user.id}</th>
+                    <td>{user.email}</td>
+                    <td>{user.isAdmin}</td>
+                    <td>
+                      <NavLink to={`users/${user.id}`} >
+                        <button type="button" className="btn btn-outline-danger">Update</button>
+                      </NavLink>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+
+        <font size='5'>Products</font>
+        <table id="productTable" className="table">
+          <thead>
+            <tr>
+              <th>ID #</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Update</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.props.products.map(product => {
+                return (
+                  <tr key={product.id}>
+                    <th scope="row">{product.id}</th>
+                    <td>{product.name}</td>
+                    <td>{product.price}</td>
+                    <td>{product.quantity}</td>
+                    <td>
+                      <NavLink to={`product/${product.id}/edit`} >
+                        <button type="button" className="btn btn-outline-danger">Update</button>
+                      </NavLink>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+
+        <font size='5'>Orders</font>
+        <table id="orderTable" className="table">
+          <thead>
+            <tr>
+              <th>ID #</th>
+              <th>Status</th>
+              <th>Checkout Date</th>
+              <th>Update</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.props.orders.map(order => {
+                return (
+                  <tr key={order.id}>
+                    <th scope="row">{order.id}</th>
+                    <td>{order.status}</td>
+                    <td>{order.checkoutDate}</td>
+                    <td>
+                      <NavLink to={`orders/${order.id}/edit`} >
+                        <button type="button" className="btn btn-outline-danger">Update</button>
+                      </NavLink>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
@@ -113,6 +123,15 @@ const mapStateToProps = (state, ownProps) => {
     users: state.users,
     orders: state.orders,
     products: state.products
+  }
+}
+
+const mapDispatch = (dispatch) => {
+  return {
+    loadInitialData () {
+      dispatch(fetchOrders())
+      dispatch(fetchUsers())
+    }
   }
 }
 
@@ -130,4 +149,4 @@ const mapStateToProps = (state, ownProps) => {
 //   };
 // };
 
-export default connect(mapStateToProps)(AdminPortal)
+export default connect(mapStateToProps, mapDispatch)(AdminPortal)
