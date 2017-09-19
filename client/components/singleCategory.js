049e7da2b10ef878b3b2f353052b05ecd1d0e6d7
David Eiber <davideiber@gmail.com>
2017-09-19 14:33:12 -0400

    Add singleCategory functionality

---
+++ client/components/singleCategory.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import {ReviewForm} from './index';
+import SingleProduct from './singleProduct'
+// import Category from "../store/category";
+
+function SingleCategory(props) {
+  const { categories } = props;
+  const categoryId = props.match.params.categoryId;
+  console.log("props", props);
+  console.log("categoryId ", categoryId);
+  let currentCategory = categories.filter(category => {
+    return category.id == categoryId;
+  })[0];
+
+  let prodsinCat = currentCategory ? currentCategory.products : [];
+  console.log("prodsinCat", prodsinCat);
+
+  return (
+	<div className="row container-fluid">
+      {/* do category checking below? cant render the all products component in main instead */}
+      {
+        prodsinCat.map(product => {
+          return (
+            <div className="single-product col-xs-3" key={product.id}>
+              <SingleProduct product={product}/>
+            </div>
+          )
+        })
+      }
+      <br />
+      </div>
+  );
+}
+
+const mapStateToProps = state => {
+  return {
+    categories: state.categories,
+    products: state.products
+  };
+};
+
+export default connect(mapStateToProps)(SingleCategory);
