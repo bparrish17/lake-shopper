f45896e994db8b6ec532184056895bab7d464b73
Brian Parrish <brian.parrish3739@gmail.com>
2017-09-19 20:49:16 -0400

    finished single user component

--- client/components/singleUser.js
+++ client/components/singleUser.js
@@ -21,6 +21,7 @@ class SingleUser extends Component {
         let info = user.user;
         let reviews = user.reviews;
         let orders = user.orders;
+        console.log(orders);
 
         return (
             <div>
@@ -34,7 +35,8 @@ class SingleUser extends Component {
                 <div id="user-reviews" className="col-xs-5">
                     {user.reviews
                     ? <div>
-                        <h3>My Reviews</h3>
+                        <h3><strong>My Reviews</strong></h3>
+                        <hr />
                         {   reviews.length
                             ? reviews.map(review => {
                                 return (
@@ -47,6 +49,35 @@ class SingleUser extends Component {
                                 There Are No Reviews For This Product
                             </div>
                         })
+                    </div>
+                    : <div></div>
+                    }
+                </div>
+                <div className="col-xs-1">
+                </div>
+                <div className="col-xs-5">
+                {user.orders
+                    ? <div>
+                        <h3><strong>My Orders</strong></h3>
+                        <hr />
+                        {   orders.length
+                            ? orders.map(order => {
+                                return (
+                                    <div key={order.id} className="card">
+                                        <div className="card-block">
+                                            <h4 className="card-title"><i>Order #{order.id}</i></h4>
+                                            <h5><strong>Shipping Address: </strong>{order.address}</h5>
+                                            <h5><strong>Status: </strong>{order.status}</h5>
+                                            <h5><strong>Checkout Date: </strong>{order.checkoutDate.split('T')[0].toString()}</h5>
+                                        </div>
+                                        <hr />
+                                    </div>
+                                )
+                            })
+                            : <div className="alert alert-info" role="alert">
+                                There Are No Reviews For This Product
+                            </div>
+                        }
                     </div>
                     : <div></div>
                     }
